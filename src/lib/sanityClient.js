7da4200baea4804d665d1a9c75b1f6775d088a82
Abdul-Ahad-Ali <123318968+AbdulAhad210904@users.noreply.github.com>
2025-04-04 15:49:02 +0500

    Add Ezat Database

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client"
 
 export const client = createClient({
-  projectId: 'gop155j8',
+  projectId: '76jk1b04',
   dataset: process.env.REACT_APP_SANITY_DATASET || "production",
   apiVersion: "2023-03-01",
   useCdn: false,
